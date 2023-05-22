3dcda16d8b797b09ad845a02b11b85bda4d9175c
BlckSoftware <huseyinkarayazim@gmail.com>
2023-05-22 11:31:46 +0300

    OEM eklendi

--- Mailsender.cs
+++ Mailsender.cs
@@ -3,7 +3,52 @@ namespace MailSender
 {
     public class Mailsender
     {
-        public void Yandex(string _To, string _cc, string _subject, string _content, string _smtphost, int _smtpport, string _email, string _pass)
+        public void Yandex(string _To, string _cc, string _subject, string _content, string _email, string _pass)
+        {
+            try
+            {
+                var _defaultCredentials = false;
+                var _enableSsl = true;
+                using (var smtpClient = new System.Net.Mail.SmtpClient("smtp.yandex.com",587 ))
+                {
+                    smtpClient.EnableSsl = _enableSsl;
+                    smtpClient.UseDefaultCredentials = _defaultCredentials;
+                    smtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+
+                    if (_defaultCredentials == false)
+                    {
+                        smtpClient.Credentials = new System.Net.NetworkCredential(_email, _pass);
+                    }
+                    smtpClient.Send(_email, _To, _subject, _content);
+                }
+            }
+            catch (Exception)
+            { throw; }
+        }
+
+        public void Outlook(string _To, string _cc, string _subject, string _content, string _email, string _pass)
+        {
+            try
+            {
+                System.Net.Mail.MailMessage Msg = new System.Net.Mail.MailMessage();
+                System.Net.Mail.SmtpClient Client = new System.Net.Mail.SmtpClient();
+                Client.Port = 587;
+                Client.Host = "smtp-mail.outlook.com";
+                Client.EnableSsl = true;
+                Client.Credentials = new System.Net.NetworkCredential(_email, _pass);
+                Msg.To.Add(_To);
+                Msg.From = new System.Net.Mail.MailAddress(_email);
+                Msg.Subject = (_subject);
+                Msg.Body = _content;
+                Client.Send(Msg);
+
+
+            }
+            catch (Exception)
+            { throw; }
+
+        }
+        public void OEM(string _To, string _cc, string _subject, string _content, string _smtphost, int _smtpport, string _email, string _pass)
         {
             try
             {
@@ -26,28 +71,6 @@ namespace MailSender
             { throw; }
         }
 
-        public void Outlook(string _To, string _cc, string _subject, string _content, string _smtphost, int _smtpport, string _email, string _pass)
-        {
-            try
-            {
-                System.Net.Mail.MailMessage Msg = new System.Net.Mail.MailMessage();
-                System.Net.Mail.SmtpClient Client = new System.Net.Mail.SmtpClient();
-                Client.Port = _smtpport;
-                Client.Host = _smtphost;
-                Client.EnableSsl = true;
-                Client.Credentials = new System.Net.NetworkCredential(_email, _pass);
-                Msg.To.Add(_To);
-                Msg.From = new System.Net.Mail.MailAddress(_email);
-                Msg.Subject = (_subject);
-                Msg.Body = _content;
-                Client.Send(Msg);
-
-
-            }
-            catch (Exception)
-            { throw; }
-
-        }
     }
 }
 
